4c2d4ed6d076cd0e6ab619d5cc34a9f2e1ca1e6c
DesDevCharan <charanbabukarnam@gmail.com>
2020-05-18 11:49:43 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,5 +2,26 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// [...'John Resig'] // log the output
+
+
+// 'use strict';
+// var n = 022;
+
+
+// const circle = { radius: 20, diameter() { return this.radius * 2; }, perimeter: () => 2 * Math.PI * this.radius }; --- no 
+
+// console.log(circle.diameter()); console.log(circle.perimeter());
+
+
+// console.log(![]);
+
+// sum(1)(2)(3)- ---sum(1,2,3)
+
+
+// singleton -design
+
+// prototypal inheritance --- mcafee extends security
+
+// iife using arrow func
